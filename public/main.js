ab6f1f31680d753d27ca8557c5a0388e5c933153
Justin Swiger <justin.swiger@me.com>
2019-11-06 10:07:48 -0500

    finished timer

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const qs = (element) => document.querySelector(element)
+
+let timer = 0
+let interval = null
+
+const startTimer = () => {
+  if (!interval) {
+    interval = setInterval(() => {
+      timer++
+      // console.log('heart beat', timer)
+      qs('.seconds').textContent = timer
+      let seconds = timer
+      if (timer < 10) {
+        seconds = '0' + timer.toString()
+      }
+      qs('.seconds').textContent = seconds
+    }, 1000)
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const stopTimer = () => {
+  // console.log(interval)
+  clearInterval(interval)
+  interval = null
+}
+
+qs('.start-button').addEventListener('click', startTimer)
+qs('.stop-button').addEventListener('click', stopTimer)
